fe21a6b631451cfac18d85e7e071936aa7660edb
SushantaKumarSahoo <111504191+SushantaKumarSahoo@users.noreply.github.com>
2025-05-31 20:48:34 +0530

    mouse-pointer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ import { Header } from "@/sections/Header";
 import { HeroSection } from "@/sections/Hero";
 import { ProjectsSection } from "@/sections/Projects";
 import {SnackbarProvider} from "notistack"
+import { SmoothCursor } from "@/components/ui/smooth-cursor";
 
 export default function Home() {
   return (
@@ -19,6 +20,7 @@ export default function Home() {
       maxSnack={1}
     >
       <div>
+      <SmoothCursor />
       <Header />
       <HeroSection />
       <AboutSection />
